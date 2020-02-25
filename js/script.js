b66943c3021d4d5b9c4b5c3cec4e0942540fb031
mileto1975 <mileto.di.marco@howest.be>
2020-02-25 17:53:34 +0100

    Add finish instructions

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
  * ^ = machtsverheffing = Math.pow()
  * Splits zo veel mogelijk logica in in functies
  * Vul het table element met rijen en kolommen
+ * splits de functies af in een afzonderlijke function file
  */
 //declare global vars here
 window.addEventListener('load',Initialize);
