59507b9bd597b5281de0d838bdee7d0ebd855e3e
mileto1975 <mileto.di.marco@howest.be>
2020-02-26 12:34:58 +0100

    Add extra instructions

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
  * annuïteit = (maandrente / (1 - ((1 + maandrente) ^ - aantal periodes))) * hypotheekbedrag
  * ^ = machtsverheffing = Math.pow()
  * Splits zo veel mogelijk logica in in functies
- * Vul het table element met rijen en kolommen
+ * Vul het table element(innerHTML) met rijen en kolommen
  * splits de functies af in een afzonderlijke function file
  */
 //declare global vars here
